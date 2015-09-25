3d83ba8328a07fdef952592ade0c5f81f4adb5fc
rimihaeg <rimihaeg@live.nl>
2015-09-25 13:01:30 +0200

    Minesweeper map overzicht

---
+++ src/Minesweeper/Main.java
@@ -0,0 +1,16 @@
+package Minesweeper;
+
+/**
+ * Created by Rick on 25-9-2015.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        new Main().run();
+    }
+
+    public void run(){
+
+    }
+
+}
